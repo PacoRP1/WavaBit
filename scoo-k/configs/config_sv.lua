7adff7fdd4c2f0336582b2397b84e810962be8bb
PacoRP1 <86936729+PacoRP1@users.noreply.github.com>
2021-08-06 00:17:55 +0200

    Add files via upload

---
+++ scoo-k/configs/config_sv.lua
@@ -0,0 +1,13 @@
+ConfigACS = {}
+
+ConfigACS.ModelsLogWebhook = "https://discord.com/api/webhooks/790699756703187008/agOYIBx9opxKiVrWSwdnrgEPSxul5lcAgXsgITPFxqWcWMjM9SmXQzbwUtrouHW2-uwQ"
+ConfigACS.ExplosionLogWebhook = "https://discord.com/api/webhooks/790699756703187008/agOYIBx9opxKiVrWSwdnrgEPSxul5lcAgXsgITPFxqWcWMjM9SmXQzbwUtrouHW2-uwQ"
+
+ConfigACS.LogBanWebhook = "https://discord.com/api/webhooks/790699756703187008/agOYIBx9opxKiVrWSwdnrgEPSxul5lcAgXsgITPFxqWcWMjM9SmXQzbwUtrouHW2-uwQ" --YOUR BANS/LOGS WEBHOOK
+ConfigACS.AntiVPNWebhook = "https://discord.com/api/webhooks/790699756703187008/agOYIBx9opxKiVrWSwdnrgEPSxul5lcAgXsgITPFxqWcWMjM9SmXQzbwUtrouHW2-uwQ" -- ANTIVPN WEBHOOK
+
+ConfigACS.ServerName = "KaliaRP" --YOUR SERVER NAME
+
+ConfigACS.Version = "v1.6.4" -- DONT TOUCH THIS
+
+ConfigACS.License = "CRACKED"
